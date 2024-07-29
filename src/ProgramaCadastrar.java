3c94807298c1dd6feee42c415d62347a14fb0ec8
Daniel Araújo <danielaraujo.fs@gmail.com>
2024-07-29 09:53:23 -0300

    Exemplo final

---
+++ src/ProgramaCadastrar.java
@@ -0,0 +1,42 @@
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import dominio.Produto;
+
+public class ProgramaCadastrar {
+	
+	private static EntityManagerFactory emf;
+
+	private static EntityManager getEntityManager() {
+		 if (emf == null) {
+			emf = Persistence.createEntityManagerFactory("produtos");
+		 }
+		 return emf.createEntityManager();
+	}
+
+	public static void main(String args[]) {
+		
+		Produto produto1 = new Produto();
+		produto1.setNome("Computador");
+		produto1.setQuantidade(35);
+		
+		Produto produto2 = new Produto();
+		produto2.setNome("Mouse");
+		produto2.setQuantidade(100);
+		
+		EntityManager em = getEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(produto1);
+		em.persist(produto2);
+		em.getTransaction().commit(); 
+		
+		em.close();
+		emf.close();
+	}
+	
+}
